64a447aaa9484e0c656b4441c6057128dba9685e
CaoMeiYouRen <996881204@qq.com>
2020-10-22 21:12:58 +0800

    init: 完成初始化

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'App',
+}
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+export default {
+    open: true, // 自动打开浏览器
+    port: 4000, // 设置端口
+    assetsDir: 'assets'
+}
